af7f5e5418d2c496140eabc0453b78a2838eb059
Bùi Minh Thắng <158177002+mthang045@users.noreply.github.com>
2025-11-30 22:36:20 +0700

    fix debug nho nhỏ

--- backend/gen-hash.js
+++
@@ -1,7 +0,0 @@
-// Script to generate bcrypt hash for 'admin123'
-const bcrypt = require('bcryptjs');
-
-const password = 'admin123';
-bcrypt.hash(password, 10).then(hash => {
-  console.log('Hash for admin123:', hash);
-});
